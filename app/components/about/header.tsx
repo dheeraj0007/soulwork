e90e85825625683b22d540b4c01f6ce482b6f304
dheeraj0007 <dheeraj1863.be20@chitkara.edu.in>
2025-09-07 22:39:57 +0530

    header image added

--- app/components/about/header.tsx
+++ app/components/about/header.tsx
@@ -3,7 +3,15 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="bg-[var(--pre-primary)]/10 flex items-center justify-center"></div>
+    <div className="bg-[var(--pre-primary)] flex items-center justify-center">
+      <Image
+        src={"/about/aboutheader2.png"}
+        width={1000}
+        height={1000}
+        alt="header"
+        className="md:h-screen h-fit mt-10"
+      />
+    </div>
   );
 }
 
--- app/components/about/intro.tsx
+++ app/components/about/intro.tsx
@@ -15,7 +15,7 @@ function Intro() {
       initial={{ opacity: 0, y: 30 }}
       animate={isClient ? { opacity: 1, y: 0 } : { opacity: 0, y: 30 }}
       transition={{ duration: 1, ease: "easeOut" }}
-      className="py-14 px-8 flex items-center justify-center mt-18 bg-[var(--pre-primary)]/20"
+      className="py-14 px-8 flex items-center justify-center bg-[var(--pre-primary)]/20"
     >
       <p className="text-center w-full text-xl md:w-2/3 leading-10 tracking-tight">
         I&apos;m Adi, the visionary founder and guiding force behind
